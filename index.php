83f6dac535ffcddd51ab5520c54dde632f83869d
WangHaoRan <whrlmc@139.com>
2016-08-02 17:12:41 +0800

    2
    w

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
  * furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * all copies or substanti啊实打实的al portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
